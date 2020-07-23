e17527634f19f9e135005023cd20ba8ae99a0d26
Ciaran Bent <ciaran.bent@protonmail.ch>
2020-07-23 23:33:46 +0100

    Updated Script;
    Signed-off-by: Ciaran Bent <ciaran.bent@protonmail.ch>

--- src/js/such_a_shame_there_is_no.js
+++ src/js/such_a_shame_there_is_no.js
@@ -1,1 +1,20 @@
-console.log("This is a test, and it worked.")
+class Person {
+    constructor() {
+        this.orig_time = 1595543241825;
+    }
+
+    CountTime(new_time) {
+        return (new_time - this.orig_time);
+    }
+}
+
+function main() {
+    console.log(`If you are seeing this, then somehow the JavaScript has worked.\n`);
+
+    let p = new Person();
+    let days = p.CountTime(Date.now()) / 86400000;
+
+    console.log(`Hello, from ${days.toFixed(2)} Days ago!`);
+}
+
+main();
